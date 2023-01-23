eb28dd10932a10116b6db8525a448daad1b45e7e
ajit2599 <godeajit71@gmail.com>
2023-01-23 22:42:19 +0530

    added

---
+++ CoreJava/src/com/example/java/day2.java
@@ -0,0 +1,43 @@
+package com.example.java;
+
+public class day2 {
+
+	public static void main(String[] args) {
+		//for loop 
+		
+		//print 1 to5
+		
+		for(int i=1;i<=5;i++) {
+			//System.out.println(i);
+		}
+		
+		System.out.println("// print 5 to 1");
+		// print 5 to 1
+		
+		for(int i=5;i>=1;i--) {
+			//System.out.println(i);
+		}
+		
+		//table of 2 
+		
+		for(int i=2;i<=20;i=i+2) {
+			//System.out.println(i);
+		}
+		
+		// reverse of 2 table 
+		
+		for(int i=20;i>=2;i=i-2) {
+			System.out.println(i);
+		}
+		
+		// break with for loop 
+		
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);//1 2 3
+			if(i==2) {
+				break;
+			}
+		}
+	}
+	
+}
