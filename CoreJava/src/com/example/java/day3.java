6ec409cc970de1f636fc8f85a217ba81fe6a8a22
ajit2599 <godeajit71@gmail.com>
2023-01-25 15:31:08 +0530

    day3

---
+++ CoreJava/src/com/example/java/day3.java
@@ -0,0 +1,72 @@
+package com.example.java;
+
+public class day3 {
+
+	public static void main(String[] args) {
+		
+        //HardCoded values 
+		Person Ajit = new Person();
+		System.out.println(Ajit.firstName);
+		System.out.println(Ajit.lastName);
+		Ajit.displayName();
+		
+		
+		// objectcrreation value
+		
+		PersonB amol = new PersonB("Amol","Vetal");
+		amol.displayName();
+		
+		// using set method
+		
+		PersonC sanket = new PersonC();
+		sanket.setName("Ajit");
+		sanket.setLastName("Gode");
+		sanket.displayName();
+		
+		
+	}
+	
+
+}
+class Person {
+	String firstName = "Ajit";
+	String lastName = "Gode";
+	
+	public void displayName() {
+		System.out.println(this.firstName + " "+this.lastName);
+	}
+	
+}
+
+class PersonB{
+	String firstName;
+	String lastName;
+	
+	// constructor name is same as class name
+	public PersonB(String fn,String ln) {
+		this.firstName=fn;
+		this.lastName = ln;
+	}
+	public void displayName() {
+		System.out.println(this.firstName+" "+this.lastName);
+	}
+}
+
+// using set method 
+
+class PersonC{
+	String firstName;
+	String lastName;
+	
+	public void setName(String fn) {
+		this.firstName = fn;
+	}
+	
+	public void setLastName(String ln) {
+		this.lastName = ln;
+	}
+	
+	public void displayName() {
+		System.out.println(this.firstName+" "+this.lastName);
+	}
+}
